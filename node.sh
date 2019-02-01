14b3ff6c0cd5d0cd7c37368003567177899bf0f6
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-01 18:13:08 +0800

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,10 @@
+yum install python-setuptools && easy_install pip
+yum install git
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/libsodium.sh && chmod +x libsodium.sh && bash libsodium.sh
+git clone -b manyuser https://github.com/tangwulin/shadowsocks.git
+cd shadowsocks
+yum -y install python-devel
+yum -y install libffi-devel
+yum -y install openssl-devel
+pip install -r requirements.txt
+vi userapiconfig.py
