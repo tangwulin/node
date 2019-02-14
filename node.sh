184cb89b44121f65c42a8498a60044f983260833
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-14 17:01:57 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,29 +1,12 @@
-echo auto-node  only Centos!
-sleep 2
-echo 该脚本仅限12138私人使用！
-sleep 2
+systemctl stop firewalld.service
+systemctl disable firewalld.service
 yum install python-setuptools && easy_install pip
 yum install git
-yum -y groupinstall "Development Tools"
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
-tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
-./configure && make -j2 && make install
-echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-ldconfig
-cd root
-git clone -b manyuser https://github.com/tangwulin/shadowsocks.git
-cd shadowsocks
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/libsodium.sh && chmod +x libsodium.sh && bash libsodium.sh
+git clone -b manyuser https://github.com/tangwulin/shadowsocks.git      cd shadowsocks
 yum -y install python-devel
 yum -y install libffi-devel
 yum -y install openssl-devel
 pip install -r requirements.txt
-echo 准备好了吗？下面开始配置设置。
-sleep 5
 vi userapiconfig.py
-cd shadowsocks
-echo 准备启动调试模式……
-sleep 5
-python server.py
-systemctl stop firewalld.service
-systemctl disable firewalld.service
 cd shadowsocks
