f269daf1c1c405d210fb653512a108f8338572ad
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-14 16:19:32 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,7 @@
 echo auto-node  only Centos!
+sleep 2
 echo 该脚本仅限12138私人使用！
+sleep 2
 yum install python-setuptools && easy_install pip
 yum install git
 yum -y groupinstall "Development Tools"
@@ -16,8 +18,11 @@ yum -y install libffi-devel
 yum -y install openssl-devel
 pip install -r requirements.txt
 echo 准备好了吗？下面开始配置设置。
+sleep 5
 vi userapiconfig.py
 cd shadowsocks
+echo 准备启动调试模式……
+sleep 5
 python server.py
 systemctl stop firewalld.service
 systemctl disable firewalld.service
