eb1df82edf21aa0e4beb000aa0f90b6d8234fcee
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-17 21:09:40 +0800

    Update settingautostart.sh

--- settingautostart.sh
+++ settingautostart.sh
@@ -1,6 +1,5 @@
-cd /etc/rc.d/init.d
-wget -N --no-check-certificate https://raw.githubusercontent.com/tangwulin/node/master/autostart.sh && chmod +x autostart.sh && bash autostart.sh
-chmod +x  /etc/rc.d/init.d/autostart.sh
-cd /etc/rc.d/init.d
-chkconfig --add autostart.sh
-chkconfig autostart.sh on
+cd /etc/rc.d/
+rm -rf rc.local
+wget -N --no-check-certificate https://raw.githubusercontent.com/tangwulin/node/master/rc.local
+chmod +x /etc/rc.d/rc.local
+
