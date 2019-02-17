135bd4cd7a95e36d1aef6bf942c0c4f607ebd41d
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-17 19:12:08 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,7 +1,8 @@
 yum install python-setuptools && easy_install pip
 yum install git
 wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/libsodium.sh && chmod +x libsodium.sh && bash libsodium.sh
-git clone -b manyuser https://github.com/tangwulin/shadowsocks.git      cd shadowsocks
+git clone -b manyuser https://github.com/tangwulin/shadowsocks.git
+cd shadowsocks
 yum -y install python-devel
 yum -y install libffi-devel
 yum -y install openssl-devel
