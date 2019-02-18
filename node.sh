1c7eb28c5413260dde13adfba9da0c15b99120df
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-18 21:38:56 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -10,3 +10,9 @@ pip install -r requirements.txt
 vi userapiconfig.py
 sleep 2
 cd shadowsocks
+yum -y groupinstall "Development Tools"
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
+tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
+./configure && make -j2 && make install
+echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+ldconfig
