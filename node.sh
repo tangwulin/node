ce5034d655d41f7ca568a508e789da188479e30a
tangwulin <45118263+tangwulin@users.noreply.github.com>
2019-02-18 19:24:20 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,11 @@
 yum install python-setuptools && easy_install pip
 yum install git
-wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/libsodium.sh && chmod +x libsodium.sh && bash libsodium.sh
+yum -y groupinstall "Development Tools"
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
+tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
+./configure && make -j2 && make install
+echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+ldconfig
 git clone -b manyuser https://github.com/tangwulin/shadowsocks.git
 sleep 2
 cd shadowsocks
